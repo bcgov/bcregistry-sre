24beb78f82dbf5612a3feb9e0aa7d8efb2cc3893
Ali Hamood <ali.hamood@gov.bc.ca>
2025-06-16 15:23:04 -0700

    Moved Cloud Task module into infra folder. Adds cloud_tasks and slo folders in infra folder. Adds new cloud task backend based on parent level backend. Adds configs for all 4 project types and configurse cloud tasks queues for a083gt-*

--- gcp/terraform/infra/cloud_tasks/outputs.tf
+++ gcp/terraform/infra/cloud_tasks/outputs.tf
@@ -11,4 +11,9 @@ output "queue_id" {
 output "queue_location" {
   description = "The location of the Cloud Tasks queue"
   value       = google_cloud_tasks_queue.cloud_tasks_queue.location
-} 
+}
+
+output "queue_state" {
+  description = "The current state of the Cloud Tasks queue"
+  value       = google_cloud_tasks_queue.cloud_tasks_queue.state
+}
