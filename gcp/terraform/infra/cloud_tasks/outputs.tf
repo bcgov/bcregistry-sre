221a97fc5395af6ea24d6dd135bcfe6a90b05b79
Ali Hamood <ali.hamood@gov.bc.ca>
2025-06-17 15:06:29 -0700

    Updates output.tf to iterate over outputs and print the name, id, and location. This is necessary because this can create multiple cloud tasks. Also removes status output because no such variable is used in the implementation anymore.

--- gcp/terraform/infra/cloud_tasks/outputs.tf
+++ gcp/terraform/infra/cloud_tasks/outputs.tf
@@ -1,19 +1,8 @@
-output "queue_name" {
-  description = "The name of the Cloud Tasks queue"
-  value       = google_cloud_tasks_queue.cloud_tasks_queue.name
-}
-
-output "queue_id" {
-  description = "The ID of the Cloud Tasks queue"
-  value       = google_cloud_tasks_queue.cloud_tasks_queue.id
-}
-
-output "queue_location" {
-  description = "The location of the Cloud Tasks queue"
-  value       = google_cloud_tasks_queue.cloud_tasks_queue.location
-}
-
-output "queue_state" {
-  description = "The current state of the Cloud Tasks queue"
-  value       = google_cloud_tasks_queue.cloud_tasks_queue.state
+output "instances" {
+  description = "The name, id, and location of the Cloud Tasks queue"
+  value       = { for k, v in google_cloud_tasks_queue.cloud_tasks_queue : k => {
+    name = v.name
+    id = v.id
+    location = v.location
+  } }
 }
