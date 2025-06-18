429d45dc39b3653717253835992ad718ce50f963
Ali Hamood <ali.hamood@gov.bc.ca>
2025-06-18 11:51:26 -0700

    Fixed miss-match between varables.tf and _config_project_*.auto.tfvars.

--- gcp/terraform/infra/cloud_tasks/variables.tf
+++ gcp/terraform/infra/cloud_tasks/variables.tf
@@ -13,19 +13,17 @@ variable "dev_projects" {
   type = map(object({
     project_id = string
     env        = string
-    cloud_tasks = optional(map(object({
-      instances = list(object({
-        instance = string
-        max_dispatches_per_second = optional(number, 500)
-        max_concurrent_dispatches = optional(number, 100)
-        max_attempts = optional(number, 3)
-        max_retry_duration = optional(string, "3600s")
-        min_backoff = optional(string, "0.1s")
-        max_backoff = optional(string, "3600s")
-        max_doublings = optional(number, 16)
-        sampling_ratio = optional(number, 1.0)
-      }))
-    })), {})
+    instances = list(object({
+      instance = string
+      max_dispatches_per_second = optional(number, 500)
+      max_concurrent_dispatches = optional(number, 100)
+      max_attempts = optional(number, 3)
+      max_retry_duration = optional(string, "3600s")
+      min_backoff = optional(string, "0.1s")
+      max_backoff = optional(string, "3600s")
+      max_doublings = optional(number, 16)
+      sampling_ratio = optional(number, 1.0)
+    }))
   }))
   default = {}
   description = "Configuration for development projects and their Cloud Tasks queues"
@@ -35,19 +33,17 @@ variable "test_projects" {
   type = map(object({
     project_id = string
     env        = string
-    cloud_tasks = optional(map(object({
-      instances = list(object({
-        instance = string
-        max_dispatches_per_second = optional(number, 500)
-        max_concurrent_dispatches = optional(number, 100)
-        max_attempts = optional(number, 3)
-        max_retry_duration = optional(string, "3600s")
-        min_backoff = optional(string, "0.1s")
-        max_backoff = optional(string, "3600s")
-        max_doublings = optional(number, 16)
-        sampling_ratio = optional(number, 1.0)
-      }))
-    })), {})
+    instances = list(object({
+      instance = string
+      max_dispatches_per_second = optional(number, 500)
+      max_concurrent_dispatches = optional(number, 100)
+      max_attempts = optional(number, 3)
+      max_retry_duration = optional(string, "3600s")
+      min_backoff = optional(string, "0.1s")
+      max_backoff = optional(string, "3600s")
+      max_doublings = optional(number, 16)
+      sampling_ratio = optional(number, 1.0)
+    }))
   }))
   default = {}
   description = "Configuration for development projects and their Cloud Tasks queues"
@@ -57,19 +53,17 @@ variable "prod_projects" {
   type = map(object({
     project_id = string
     env        = string
-    cloud_tasks = optional(map(object({
-      instances = list(object({
-        instance = string
-        max_dispatches_per_second = optional(number, 500)
-        max_concurrent_dispatches = optional(number, 100)
-        max_attempts = optional(number, 3)
-        max_retry_duration = optional(string, "3600s")
-        min_backoff = optional(string, "0.1s")
-        max_backoff = optional(string, "3600s")
-        max_doublings = optional(number, 16)
-        sampling_ratio = optional(number, 1.0)
-      }))
-    })), {})
+    instances = list(object({
+      instance = string
+      max_dispatches_per_second = optional(number, 500)
+      max_concurrent_dispatches = optional(number, 100)
+      max_attempts = optional(number, 3)
+      max_retry_duration = optional(string, "3600s")
+      min_backoff = optional(string, "0.1s")
+      max_backoff = optional(string, "3600s")
+      max_doublings = optional(number, 16)
+      sampling_ratio = optional(number, 1.0)
+    }))
   }))
   default = {}
   description = "Configuration for development projects and their Cloud Tasks queues"
@@ -79,19 +73,17 @@ variable "other_projects" {
   type = map(object({
     project_id = string
     env        = string
-    cloud_tasks = optional(map(object({
-      instances = list(object({
-        instance = string
-        max_dispatches_per_second = optional(number, 500)
-        max_concurrent_dispatches = optional(number, 100)
-        max_attempts = optional(number, 3)
-        max_retry_duration = optional(string, "3600s")
-        min_backoff = optional(string, "0.1s")
-        max_backoff = optional(string, "3600s")
-        max_doublings = optional(number, 16)
-        sampling_ratio = optional(number, 1.0)
-      }))
-    })), {})
+    instances = list(object({
+      instance = string
+      max_dispatches_per_second = optional(number, 500)
+      max_concurrent_dispatches = optional(number, 100)
+      max_attempts = optional(number, 3)
+      max_retry_duration = optional(string, "3600s")
+      min_backoff = optional(string, "0.1s")
+      max_backoff = optional(string, "3600s")
+      max_doublings = optional(number, 16)
+      sampling_ratio = optional(number, 1.0)
+    }))
   }))
   default = {}
   description = "Configuration for development projects and their Cloud Tasks queues"
